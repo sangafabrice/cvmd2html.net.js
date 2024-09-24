1b8177a9e09649589d31335f926f4bbae8bef46f
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-09-24 12:39:34 +0100

    refactor: Centralize COM object initialization

--- src/package.js
+++ src/package.js
@@ -1,7 +1,7 @@
 /**
  * @file returns information about the resource files used by the project.
  * It also provides a way to manage the custom icon link that can be installed and uninstalled.
- * @version 0.0.1.3
+ * @version 0.0.1.4
  */
 
 /**
@@ -19,48 +19,37 @@
 
 /** @class */
 var Package = (function() {
-  var fs = new ActiveXObject('Scripting.FileSystemObject');
   var resource = {
-    Root: fs.GetParentFolderName(AssemblyLocation),
+    Root: FileSystem.GetParentFolderName(AssemblyLocation),
     MenuIconPath: AssemblyLocation,
     PwshExePath: (function() {
-      var registry = GetObject('winmgmts:StdRegProv');
-      var getStringValueMethod = registry.Methods_.Item('GetStringValue');
+      var getStringValueMethod = StdRegProv.Methods_.Item('GetStringValue');
       var inParam = getStringValueMethod.InParameters.SpawnInstance_();
       // The HKLM registry subkey stores the PowerShell Core application path.
       inParam.sSubKeyName = 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\pwsh.exe';
       try {
-        return registry.ExecMethod_(getStringValueMethod.Name, inParam).sValue;
+        return StdRegProv.ExecMethod_(getStringValueMethod.Name, inParam).sValue;
       } finally {
         Marshal.FinalReleaseComObject(getStringValueMethod);
         Marshal.FinalReleaseComObject(inParam);
-        Marshal.FinalReleaseComObject(registry);
-        registry = null;
         inParam = null;
         getStringValueMethod = null;
       }
-    })(),
-    /**
-     * Destroy the object.
-     */
-    Dispose: function () {
-      Marshal.FinalReleaseComObject(fs);
-      fs = null;
-    }
+    })()
   }
-  resource.ResourcePath = fs.BuildPath(resource.Root, 'rsc');
-  resource.PwshScriptPath = fs.BuildPath(resource.ResourcePath, 'cvmd2html.ps1');
+  resource.ResourcePath = FileSystem.BuildPath(resource.Root, 'rsc');
+  resource.PwshScriptPath = FileSystem.BuildPath(resource.ResourcePath, 'cvmd2html.ps1');
   resource.IconLink = {
-    DirName: (new ActiveXObject('WScript.Shell')).ExpandEnvironmentStrings('%TEMP%'),
-    Name: (new ActiveXObject('Scriptlet.TypeLib')).Guid.substr(1, 36).toLowerCase() + '.tmp.lnk',
+    DirName: WshShell.ExpandEnvironmentStrings('%TEMP%'),
+    Name: Scriptlet.Guid.substr(1, 36).toLowerCase() + '.tmp.lnk',
     /**
      * Create the custom icon link file.
      * @method @memberof resource.IconLink
      * @param {string} markdownPath is the input markdown file path.
      */
     Create: function (markdownPath) {
-      fs.CreateTextFile(this.Path).Close();
-      var link = (new ActiveXObject('Shell.Application')).NameSpace(this.DirName).ParseName(this.Name).GetLink;
+      FileSystem.CreateTextFile(this.Path).Close();
+      var link = Shell.NameSpace(this.DirName).ParseName(this.Name).GetLink;
       link.Path = resource.PwshExePath;
       link.Arguments = format('-ep Bypass -nop -w Hidden -f "{0}" -Markdown "{1}"', [resource.PwshScriptPath, markdownPath]);
       link.SetIconLocation(resource.MenuIconPath, 0);
@@ -74,10 +63,10 @@ var Package = (function() {
      */
     Delete: function () {
       try {
-        fs.DeleteFile(this.Path);
+        FileSystem.DeleteFile(this.Path);
       } catch (error) { }
     }
   }
-  resource.IconLink.Path = fs.BuildPath(resource.IconLink.DirName, resource.IconLink.Name);
+  resource.IconLink.Path = FileSystem.BuildPath(resource.IconLink.DirName, resource.IconLink.Name);
   return resource;
 })();
--- version.h
+++ version.h
@@ -1,2 +1,2 @@
-#define VER_VERSION 0,0,1,8
-#define VER_VERSION_STR "0.0.1.8"
+#define VER_VERSION 0,0,1,15
+#define VER_VERSION_STR "0.0.1.15"
