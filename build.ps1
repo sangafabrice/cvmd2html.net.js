20ace4d4b7d5bc1b8bd5348e59b5ceba04e5cf31
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-09-24 23:54:09 +0100

    feat(milestone): Convert using a JavaScript library

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-<#PSScriptInfo .VERSION 0.0.1.4#>
+<#PSScriptInfo .VERSION 0.0.1.5#>
 
 using namespace System.Management.Automation
 [CmdletBinding()]
@@ -24,7 +24,7 @@ Param ()
   }
   New-Item $BinDir -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
   Copy-Item "$PSScriptRoot\rsc" -Destination $BinDir -Recurse
-  Copy-Item "$PSScriptRoot\cvmd2html.psd1" -Destination $BinDir
+  Copy-Item $(C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command '[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.mshtml").Location') -Destination ($MshtmlDllPath = "$BinDir\Microsoft.mshtml.dll") -Force
 
   Function ImportTypeLibrary {
     <#
@@ -64,7 +64,7 @@ Param ()
   # Compile the source code with jsc.
   $EnvPath = $Env:Path
   $Env:Path = "$Env:windir\Microsoft.NET\Framework$(If ([Environment]::Is64BitOperatingSystem) { '64' })\v4.0.30319\;$Env:Path"
-  jsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32res:$(CompileResource 'cvmd2html') /reference:$(ImportTypeLibrary 'C:\Windows\System32\wbem\wbemdisp.tlb' 'WbemScripting') /reference:$(ImportTypeLibrary 'C:\Windows\System32\wshom.ocx' 'IWshRuntimeLibrary') /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.js" "$SrcDir\conhost.js" "$SrcDir\msgbox.js" "$SrcDir\package.js" "$SrcDir\parameters.js" "$PSScriptRoot\index.js" "$SrcDir\StdRegProv.js" "$SrcDir\setup.js" "$SrcDir\utils.js"
+  jsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32res:$(CompileResource 'cvmd2html') /reference:$MshtmlDllPath /reference:$(ImportTypeLibrary 'C:\Windows\System32\wbem\wbemdisp.tlb' 'WbemScripting') /reference:$(ImportTypeLibrary 'C:\Windows\System32\wshom.ocx' 'IWshRuntimeLibrary') /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.js" "$SrcDir\converter.js" "$SrcDir\msgbox.js" "$SrcDir\package.js" "$SrcDir\parameters.js" "$PSScriptRoot\index.js" "$SrcDir\StdRegProv.js" "$SrcDir\setup.js" "$SrcDir\utils.js"
   $Env:Path = $EnvPath
 
   If ($LASTEXITCODE -eq 0) {
---
+++ src/converter.js
@@ -0,0 +1,133 @@
+/**
+ * @file returns the method to convert from markdown to html.
+ * @version 0.0.1.2
+ */
+
+/**
+ * Represents the markdown to html converter.
+ * @typedef {object} MarkdownToHtml
+ */
+
+var CreateConverter = (function() {
+  /** @constant {int32} */
+  var PERMISSION_DENIED = int(0x800A0046);
+  /** @constant {int32} */
+  var FILE_NOT_FOUND = int(0x800A0035);
+
+  /** @constant {regexp} */
+  var MARKDOWN_REGEX = /\.md$/i;
+
+  /**
+   * @param {string} htmlLibraryPath is the path string of the html loading the library.
+   * @param {string} jsLibraryPath is the javascript library path.
+   * @returns {object} the Converter type.
+   */
+  return function (htmlLibraryPath, jsLibraryPath) {
+    /** @class @constructs MarkdownToHtml */
+    function MarkdownToHtml() { }
+
+    /**
+     * Convert the content of the markdown file and write it to an html file.
+     * @public @static @memberof MarkdownToHtml
+     * @param {string} markdownPath is the markdown file path.
+     */
+    MarkdownToHtml.ConvertFrom = function (markdownPath) {
+      // Validate the input markdown path string.
+      if (!MARKDOWN_REGEX.test(markdownPath)) {
+        MessageBox.Show(format('"{0}" is not a markdown (.md) file.', markdownPath));
+      }
+      SetHtmlContent(GetHtmlPath(markdownPath), ConvertToHtml(GetContent(markdownPath)));
+    }
+
+    /**
+     * Convert a markdown content to an html document.
+     * @param {string} mardownContent is the content to convert.
+     * @returns {string} the output html document content.
+     */
+    function ConvertToHtml(markdownContent) {
+      // Build the HTML document that will load the showdown.js library.
+      var document = new HTMLDocumentClass();
+      document.open();
+      document.IHTMLDocument2_write(format(GetContent(htmlLibraryPath), GetContent(jsLibraryPath)));
+      document.close();
+      document.body.innerText = markdownContent;
+      document.parentWindow.execScript('convertMarkdown()', 'javascript');
+      try {
+        return document.body.innerHTML;
+      } finally {
+        if (document) {
+          Marshal.FinalReleaseComObject(document);
+          document = null;
+        }
+      }
+    }
+
+    /**
+     * This function returns the output path when it is unique without prompts or when
+     * the user accepts to overwrite an existing HTML file. Otherwise, it exits the script.
+     * @returns {string} the output html path.
+     */
+    function GetHtmlPath(markdownPath) {
+      var htmlPath = markdownPath.replace(MARKDOWN_REGEX, '.html');
+      if (FileSystem.FileExists(htmlPath)) {
+        MessageBox.Show(format('The file "{0}" already exists.\n\nDo you want to overwrite it?', htmlPath), MessageBox.WARNING);
+      } else if (FileSystem.FolderExists(htmlPath)) {
+        MessageBox.Show(format('"{0}" cannot be overwritten because it is a directory.', htmlPath));
+      }
+      return htmlPath;
+    }
+
+    /**
+     * Get the content of a file.
+     * @param {string} filePath is path that is read.
+     * @returns {string} the content of the file.
+     */
+    function GetContent(filePath) {
+      try {
+        with (FileSystem.OpenTextFile(filePath, IOMode.ForReading)) {
+          var content = ReadAll();
+          Close();
+        }
+        return content;
+      } catch (error: Exception) {
+        switch (error.HResult) {
+          case PERMISSION_DENIED:
+            if (!FileSystem.FolderExists(filePath)) {
+              MessageBox.Show(format('Access to the path "{0}" is denied.', filePath));
+            }
+          case FILE_NOT_FOUND:
+            MessageBox.Show(format('File "{0}" is not found.', filePath));
+          default:
+            MessageBox.Show(format('Unspecified error trying to read from "{0}".', filePath));
+        }
+      }
+    }
+
+    /**
+     * Write the html text to the output HTML file.
+     * It notifies the user when the operation did not complete with success.
+     * @param {string} htmlPath is the output html path.
+     * @param {string} htmlContent is the content of the html file.
+     */
+    function SetHtmlContent(htmlPath, htmlContent) {
+      try {
+        var txtStream = FileSystem.OpenTextFile(htmlPath, IOMode.ForWriting, true);
+        txtStream.Write(htmlContent);
+      } catch (error: Exception) {
+        if (error.HResult == PERMISSION_DENIED) {
+          MessageBox.Show(format('Access to the path "{0}" is denied.', htmlPath));
+        } else {
+          MessageBox.Show(format('Unspecified error trying to write to "{0}".', htmlPath));
+        }
+      } finally {
+        if (txtStream) {
+          txtStream.Close();
+          Marshal.FinalReleaseComObject(txtStream);
+          txtStream = null;
+        }
+      }
+    }
+
+    return MarkdownToHtml;
+  }
+})();
