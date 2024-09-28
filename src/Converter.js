ea324403d910facd7e6d27896387aebc071136cc
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-09-28 22:24:57 +0100

    refactor: Use the JS library path in the loader Html document

--- src/Converter.js
+++ src/Converter.js
@@ -1,6 +1,6 @@
 /**
  * @file returns the method to convert from markdown to html.
- * @version 0.0.1.6
+ * @version 0.0.1.7
  */
 
 package cvmd2html {
@@ -39,7 +39,7 @@ package cvmd2html {
         Navigate('about:blank');
         var document = Document;
       }
-      document.Write(String.Format(GetContent(_htmlLibraryPath), GetContent(_jsLibraryPath)));
+      document.Write(String.Format(GetContent(_htmlLibraryPath), _jsLibraryPath));
       return document.InvokeScript('convertMarkdown', Object[]([markdownContent]));
     }
 
