aaf5fecef8d33fbdc0bdc91ce769ec5dea3a01b1
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-04 01:50:33 +0100

    refactor: Use activator to create object

--- src/converter.js
+++ src/converter.js
@@ -1,6 +1,6 @@
 /**
  * @file returns the method to convert from markdown to html.
- * @version 0.0.1.5
+ * @version 0.0.1.6
  */
 
 /**
@@ -44,7 +44,7 @@ var CreateConverter = (function() {
      */
     function ConvertToHtml(markdownContent) {
       // Build the HTML document that will load the showdown.js library.
-      var document = Interaction.CreateObject('htmlFile');
+      var document = Activator.CreateInstance(Type.GetTypeFromProgID('htmlFile'));
       document.open();
       document.IHTMLDocument2_write(format(GetContent(htmlLibraryPath), GetContent(jsLibraryPath)));
       document.close();
