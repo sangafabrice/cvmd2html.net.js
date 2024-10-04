b3280eb087131c87184f6e6738e34cfb0c82179f
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-04 01:08:35 +0100

    feat(milestone): Convert using a JavaScript library

--- cvmd2html.psd1
+++
@@ -1,6 +0,0 @@
-@{
-  RootModule = 'rsc\cvmd2html.psm1'
-  ModuleVersion = '0.0.1.42'
-  CompatiblePSEditions = 'Core'
-  PowerShellVersion = '6.1'
-}
--- src/AssemblyInfo.js
+++ src/AssemblyInfo.js
@@ -1,14 +1,12 @@
 /**
  * @file the imports and the assembly location.
- * @version 0.0.1.10
+ * @version 0.0.1.11
  */
 
 import System.Reflection;
-import System.Diagnostics;
 import System;
 import System.IO;
 import System.Text;
-import cvmd2html;
 import System.Drawing;
 import System.Windows.Forms;
 import Microsoft.Win32;
