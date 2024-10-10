72ed39566652461bbb9c50b899d159ddb6940094
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-10 21:44:58 +0100

    feat: Initial use of PowerShell runspace

--- src/AssemblyInfo.js
+++ src/AssemblyInfo.js
@@ -1,14 +1,14 @@
 /**
  * @file the imports and the assembly location.
- * @version 0.0.1.10
+ * @version 0.0.1.13
  */
 
 import System.Reflection;
-import System.Diagnostics;
+import System.Management.Automation;
+import System.Management.Automation.Runspaces;
 import System;
 import System.IO;
 import System.Text;
-import cvmd2html;
 import System.Drawing;
 import System.Windows.Forms;
 import Microsoft.Win32;
--- src/Program.js
+++ src/Program.js
@@ -1,6 +1,6 @@
 /**
  * @file Class entry called in index.js.
- * @version 0.0.1.7
+ * @version 0.0.1.8
  */
 
 package cvmd2html {
@@ -11,8 +11,7 @@ package cvmd2html {
 
       /** The application execution. */
       if (Param.Markdown) {
-        ConsoleHost.Create(Package.PwshExePath, Package.PwshScriptPath);
-        ConsoleHost.StartWith(Param.Markdown);
+        RunRunspace(Package.PwshScriptPath, Param.Markdown);
         Quit(0);
       }
 
@@ -46,6 +45,45 @@ package cvmd2html {
     internal static function Quit(exitCode: int) {
       GC.Collect();
       Environment.Exit(exitCode);
+    }
+
+    /// <summary>Execute the target powershell script in a runspace.</summary>
+    /// <param name="pwshScriptPath">The powershell path.</param>
+    /// <param name="markdownPath">The input markdown path.</param>
+    private static function RunRunspace(pwshScriptPath: String, markdownPath: String) {
+      var runspace: Runspace = RunspaceFactory.CreateRunspace();
+      runspace.Open();
+      var powershell: PowerShell = PowerShell.Create();
+      powershell.Runspace = runspace;
+      // Execute the target PowerShell script with the markdown path argument in the runspace.
+      var asyncresult: IAsyncResult = powershell.AddCommand(pwshScriptPath).AddParameter('Markdown', markdownPath).BeginInvoke();
+      try {
+        powershell.EndInvoke(asyncresult);
+      } catch (error: ParameterBindingException) {
+        var errorText: StringBuilder = new StringBuilder(error.Message);
+        // Handle validation error on the MarkdownPath parameter.
+        if (!String.Compare(error.ParameterName, 'MarkdownPath', true) && String(errorText).StartsWith('Cannot Validate', StringComparison.InvariantCultureIgnoreCase)) {
+          var errorTextSegments: String[] = String(errorText).Split(String[](['. ']), StringSplitOptions.RemoveEmptyEntries);
+          errorText.Clear();
+          errorText.Append(errorTextSegments[0] + '. ');
+          var errorTextEnd: String = errorTextSegments[errorTextSegments.length - 1];
+          var textFormat: String;
+          if (errorTextEnd.StartsWith('Supply an argument that matches', StringComparison.InvariantCultureIgnoreCase)) {
+            textFormat = 'The extension of "{0}" is invalid. ".md" is required.';
+          } else if (errorTextEnd.StartsWith('Determine why the validation script failed', StringComparison.InvariantCultureIgnoreCase)) {
+            textFormat = 'The input file "{0}" is not found.';
+          }
+          if (!String.IsNullOrEmpty(textFormat)) {
+            MessageBox.Show(errorText.AppendFormat(textFormat, markdownPath), 'Convert to HTML', MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+          }
+        }
+        throw error;
+      } finally {
+        // Clean up
+        runspace.Close();
+        runspace.Dispose();
+      }
     }
   }
 }
