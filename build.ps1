34db5b3419013f8aa2b0329c68524cffd7da9bae
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-16 15:35:38 +0100

    refactor: Use autogenerated Resource class

--- build.ps1
+++ build.ps1
@@ -26,12 +26,13 @@ Param ()
 
   # Set the windows resources file with the resource compiler.
   & "$PSScriptRoot\rc.exe" /nologo /fo $(($ResFile = "$BinDir\resource.res")) "$PSScriptRoot\resource.rc"
-  [void] (& "$PSScriptRoot\ResGen.exe" "$PSScriptRoot\Resource.resx"  $(($TargetEmbeddedResFile = "$BinDir\Resource.resources")))
+  [void] (& "$PSScriptRoot\ResGen.exe" "$PSScriptRoot\Resource.resx"  $(($TargetEmbeddedResFile = "$BinDir\Resource.resources")) /str:jscript,cvmd2html 2>&1)
+  Rename-Item $TargetEmbeddedResFile -NewName $(($TargetEmbeddedResFile = "$BinDir\cvmd2html.Resource.resources")) -Force
 
   # Compile the source code with jsc.
   $EnvPath = $Env:Path
   $Env:Path = "$Env:windir\Microsoft.NET\Framework$(If ([Environment]::Is64BitOperatingSystem) { '64' })\v4.0.30319\;$Env:Path"
-  jsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32res:$ResFile /resource:$TargetEmbeddedResFile /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.js" "$PSScriptRoot\index.js" "$SrcDir\Program.js" "$SrcDir\Converter.js" "$SrcDir\MessageBox.js" "$SrcDir\Package.js" "$SrcDir\Param.js" "$SrcDir\Setup.js"
+  jsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32res:$ResFile /resource:$TargetEmbeddedResFile /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) $(($ResourceSourcePath = "$BinDir\Resource.js")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.js" "$PSScriptRoot\index.js" "$SrcDir\Program.js" "$SrcDir\Converter.js" "$SrcDir\MessageBox.js" "$SrcDir\Package.js" "$SrcDir\Param.js" "$SrcDir\Setup.js"
   $Env:Path = $EnvPath
 
   If ($LASTEXITCODE -eq 0) {
@@ -39,5 +40,5 @@ Param ()
     (Get-Item $ConvertExe).VersionInfo | Format-List * -Force
   }
 
-  Remove-Item "$BinDir\*.res*" -Recurse -ErrorAction SilentlyContinue
+  Remove-Item "$BinDir\*.res*",$ResourceSourcePath -Recurse -ErrorAction SilentlyContinue
 }
