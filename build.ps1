0b1b6fed81b321ffcfcd71efde0040fb06600412
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-16 15:42:02 +0100

    refactor: Use autogenerated Resource class

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-<#PSScriptInfo .VERSION 0.0.1.8#>
+<#PSScriptInfo .VERSION 0.0.1.9#>
 
 using namespace System.Management.Automation
 [CmdletBinding()]
@@ -26,14 +26,15 @@ Param ()
 
   # Set the windows resources file with the resource compiler.
   & "$PSScriptRoot\rc.exe" /nologo /fo $(($ResFile = "$BinDir\resource.res")) "$PSScriptRoot\resource.rc"
-  [void] (& "$PSScriptRoot\ResGen.exe" "$PSScriptRoot\Resource.resx"  $(($TargetEmbeddedResFile = "$BinDir\Resource.resources")))
+  [void] (& "$PSScriptRoot\ResGen.exe" "$PSScriptRoot\Resource.resx"  $(($TargetEmbeddedResFile = "$BinDir\Resource.resources")) /str:jscript,cvmd2html 2>&1)
+  Rename-Item $TargetEmbeddedResFile -NewName $(($TargetEmbeddedResFile = "$BinDir\cvmd2html.Resource.resources")) -Force
 
   # Compile the source code with jsc.
   $EnvPath = $Env:Path
   $Env:Path = "$Env:windir\Microsoft.NET\Framework$(If ([Environment]::Is64BitOperatingSystem) { '64' })\v4.0.30319\;$Env:Path"
   $CoreDll = 'C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Core\v4.0_4.0.0.0__b77a5c561934e089\System.Core.dll'
   $AutomationDll = 'C:\Windows\Microsoft.Net\assembly\GAC_MSIL\System.Management.Automation\v4.0_3.0.0.0__31bf3856ad364e35\System.Management.Automation.dll'
-  jsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32res:$ResFile /resource:$TargetEmbeddedResFile /reference:$CoreDll /reference:$AutomationDll /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.js" "$PSScriptRoot\index.js" "$SrcDir\Program.js" "$SrcDir\Package.js" "$SrcDir\Param.js" "$SrcDir\Setup.js"
+  jsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32res:$ResFile /resource:$TargetEmbeddedResFile /reference:$CoreDll /reference:$AutomationDll /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) $(($ResSourcePath = "$BinDir\Resource.js")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.js" "$PSScriptRoot\index.js" "$SrcDir\Program.js" "$SrcDir\Package.js" "$SrcDir\Param.js" "$SrcDir\Setup.js"
   $Env:Path = $EnvPath
 
   If ($LASTEXITCODE -eq 0) {
@@ -41,5 +42,5 @@ Param ()
     (Get-Item $ConvertExe).VersionInfo | Format-List * -Force
   }
 
-  Remove-Item "$BinDir\*.res*" -Recurse -ErrorAction SilentlyContinue
+  Remove-Item "$BinDir\*.res*",$ResSourcePath -Recurse -ErrorAction SilentlyContinue
 }
