e7bf89f1dcd847871a304384e9cd2d3714791cf3
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-18 13:40:51 +0100

    feat: Use Markdig package

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-<#PSScriptInfo .VERSION 0.0.1.8#>
+<#PSScriptInfo .VERSION 0.0.1.9#>
 
 using namespace System.Management.Automation
 [CmdletBinding()]
@@ -23,16 +23,16 @@ Param ()
     Return
   }
   New-Item $BinDir -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+  Copy-Item "$PSScriptRoot\App.config" -Destination "$BinDir\cvmd2html.exe.config" -Recurse
+  Copy-Item "$(($LibDir = "$PSScriptRoot\lib"))\*" -Destination $BinDir -Recurse
 
   # Set the windows resources file with the resource compiler.
   & "$PSScriptRoot\rc.exe" /nologo /fo $(($ResFile = "$BinDir\resource.res")) "$PSScriptRoot\resource.rc"
-  [void] (& "$PSScriptRoot\ResGen.exe" "$PSScriptRoot\Resource.resx"  $(($TargetEmbeddedResFile = "$BinDir\Resource.resources")) /str:jscript,cvmd2html 2>&1)
-  Rename-Item $TargetEmbeddedResFile -NewName $(($TargetEmbeddedResFile = "$BinDir\cvmd2html.Resource.resources")) -Force
 
   # Compile the source code with jsc.
   $EnvPath = $Env:Path
   $Env:Path = "$Env:windir\Microsoft.NET\Framework$(If ([Environment]::Is64BitOperatingSystem) { '64' })\v4.0.30319\;$Env:Path"
-  jsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32res:$ResFile /resource:$TargetEmbeddedResFile /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) $(($ResourceSourcePath = "$BinDir\Resource.js")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.js" "$PSScriptRoot\index.js" "$SrcDir\Program.js" "$SrcDir\Converter.js" "$SrcDir\MessageBox.js" "$SrcDir\Package.js" "$SrcDir\Param.js" "$SrcDir\Setup.js"
+  jsc.exe /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32res:$ResFile /reference:"$LibDir\Markdig.dll" /reference:"$LibDir\System.Buffers.dll" /reference:"$LibDir\System.Memory.dll" /reference:"$LibDir\System.Numerics.Vectors.dll" /reference:"$LibDir\System.Runtime.CompilerServices.Unsafe.dll" /reference:System.Numerics.dll /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.js" "$PSScriptRoot\index.js" "$SrcDir\Program.js" "$SrcDir\Converter.js" "$SrcDir\MessageBox.js" "$SrcDir\Package.js" "$SrcDir\Param.js" "$SrcDir\Setup.js"
   $Env:Path = $EnvPath
 
   If ($LASTEXITCODE -eq 0) {
@@ -40,5 +40,5 @@ Param ()
     (Get-Item $ConvertExe).VersionInfo | Format-List * -Force
   }
 
-  Remove-Item "$BinDir\*.res*",$ResourceSourcePath -Recurse -ErrorAction SilentlyContinue
+  Remove-Item "$BinDir\*.res" -Recurse -ErrorAction SilentlyContinue
 }
