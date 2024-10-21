9041ddd8466890644c9bc06b3af33a72b79fb324
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-10-21 15:14:11 +0100

    refactor: Replace WinForms with WPF MessageBox

--- src/AssemblyInfo.js
+++ src/AssemblyInfo.js
@@ -1,13 +1,12 @@
 /**
  * @file the imports and the assembly location.
- * @version 0.0.1.14
+ * @version 0.0.1.15
  */
 
 import System.Reflection;
 import System;
 import System.IO;
 import System.Text;
-import System.Drawing;
-import System.Windows.Forms;
+import System.Windows;
 import Microsoft.Win32;
 import Markdig;
--- src/MessageBox.js
+++ src/MessageBox.js
@@ -1,6 +1,6 @@
 /**
  * @file returns the markdown conversion message box.
- * @version 0.0.1.4
+ * @version 0.0.1.5
  */
 
 package cvmd2html {
@@ -8,22 +8,22 @@ package cvmd2html {
   internal abstract class MessageBox {
 
     /// <summary>The warning message type.</summary>
-    static var WARNING: MessageBoxIcon = MessageBoxIcon.Exclamation;
+    static var WARNING: MessageBoxImage = MessageBoxImage.Exclamation;
 
     /// <summary>The help message type.</summary>
-    static var HELP: MessageBoxIcon = MessageBoxIcon.None;
+    static var HELP: MessageBoxImage = MessageBoxImage.None;
 
     /// <summary>Show a warning message or an error message box.</summary>
     /// <remarks>The function quits the application if the message box is an error.</remarks>
     /// <param name="message">The message text.</param>
     /// <param name="messageType">The message box type.</param>
-    static function Show(message: String, messageType: MessageBoxIcon): String {
+    static function Show(message: String, messageType: MessageBoxImage): String {
       if (System.Array.BinarySearch(EXPECTED_MESSAGETYPE, messageType) < 0) {
         messageType = CRITICAL;
       }
       // The error message box shows the OK button alone.
       // The warning message box shows the alternative Yes or No buttons.
-      if (System.Array.BinarySearch(EXPECTED_DIALOGRESULT, System.Windows.Forms.MessageBox.Show(message, MESSAGE_BOX_TITLE, MessageBoxButtons(messageType == CRITICAL || messageType == HELP ? MessageBoxButtons.OK:MessageBoxButtons.YesNo), messageType)) >= 0) {
+      if (System.Array.BinarySearch(EXPECTED_DIALOGRESULT, System.Windows.MessageBox.Show(message, MESSAGE_BOX_TITLE, MessageBoxButton(messageType == CRITICAL || messageType == HELP ? MessageBoxButton.OK:MessageBoxButton.YesNo), messageType)) >= 0) {
         Program.Quit(1);
       }
     }
@@ -38,13 +38,13 @@ package cvmd2html {
 
     /// <summary>The list of Dialog Results that lead to quit the application.</summary>
     /// <remarks>Do not remove repetition. It is there to solve a bug.</remarks>
-    private static var EXPECTED_DIALOGRESULT: System.Array = [DialogResult.OK, DialogResult.No, DialogResult.No];
+    private static var EXPECTED_DIALOGRESULT: System.Array = [MessageBoxResult.OK, MessageBoxResult.No, MessageBoxResult.No];
 
     /// <summary>The list of expected message types.</summary>
     /// <remarks>Do not remove repetition. It is there to solve a bug.</remarks>
     private static var EXPECTED_MESSAGETYPE: System.Array = [CRITICAL, WARNING, HELP, HELP];
 
     /// <summary>The error message type.</summary>
-    private static var CRITICAL: MessageBoxIcon = MessageBoxIcon.Error;
+    private static var CRITICAL: MessageBoxImage = MessageBoxImage.Error;
   }
 }
