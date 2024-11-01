sangafabrice/cvmd2html.js-wsh

refactor: Use format to insert JS library code into Html

src/converter.js
@@ -1,6 +1,6 @@
 /**
  * @file returns the method to convert from markdown to html.
- * @version 0.0.1.2
+ * @version 0.0.1.3
  */
 
 /**
@@ -25,10 +25,11 @@
   var MARKDOWN_REGEX = /\.md$/i;
 
   /**
+   * @param {string} htmlLibraryPath is the path string of the html loading the library.
    * @param {string} jsLibraryPath is the javascript library path.
    * @returns {object} the Converter type.
    */
-  return function (jsLibraryPath) {
+  return function (htmlLibraryPath, jsLibraryPath) {
     /** @class @constructs MarkdownToHtml */
     function MarkdownToHtml() { }
 
@@ -57,23 +58,7 @@
       // Build the HTML document that will load the showdown.js library.
       var document = new ActiveXObject('htmlFile');
       document.open();
-      document.write(
-        '<!DOCTYPE html>' +
-        '<html>' +
-        '<head>' +
-          '<meta charset="UTF-8" />' +
-          '<meta http-equiv="X-UA-Compatible" content="IE=edge" />' +
-          '<script type="text/javascript">' +
-            GetContent(jsLibraryPath) +
-            'function convertMarkdown(markdownContent){' +
-              'return (new showdown.Converter()).makeHtml(markdownContent);' +
-            '}' +
-          '</script>' +
-        '</head>' +
-        '<body>' +
-        '</body>' +
-        '</html>'
-      );
+      document.write(format(GetContent(htmlLibraryPath), GetContent(jsLibraryPath)));
       document.close();
       return document.parentWindow.convertMarkdown(markdownContent);
     }

src/package.js
@@ -1,7 +1,7 @@
 /**
  * @file returns information about the resource files used by the project.
  * It also provides a way to manage the custom icon link that can be installed and uninstalled.
- * @version 0.0.1.9
+ * @version 0.0.1.11
  */
 
 /**
@@ -9,6 +9,7 @@
  * @property {string} Root is the project root path.
  * @property {string} ResourcePath is the project resources directory path.
  * @property {string} MenuIconPath is the shortcut menu icon path.
+ * @property {string} HtmlLibraryPath is the html file that embeds the JS library path.
  * @property {string} JsLibraryPath is the showdown.js library path.
  * @property {object} IconLink represents an adapted link object.
  * @property {string} IconLink.DirName is the parent directory path of the custom icon link.
@@ -23,6 +24,7 @@
     Root: fs.GetParentFolderName(WSH.ScriptFullName)
   };
   package.ResourcePath = fs.BuildPath(package.Root, 'rsc');
+  package.HtmlLibraryPath = fs.BuildPath(package.ResourcePath, 'showdown.html');
   package.JsLibraryPath = fs.BuildPath(package.ResourcePath, 'showdown.min.js');
   package.MenuIconPath = fs.BuildPath(package.ResourcePath, 'menu.ico');
   package.IconLink = {

sangafabrice/cvmd2html.net.js

src/converter.js
@@ -1,6 +1,6 @@
 /**
  * @file returns the method to convert from markdown to html.
- * @version 0.0.1.4
+ * @version 0.0.1.5
  */
 
 /**
@@ -18,10 +18,11 @@ var CreateConverter = (function() {
   var MARKDOWN_REGEX = /\.md$/i;
 
   /**
+   * @param {string} htmlLibraryPath is the path string of the html loading the library.
    * @param {string} jsLibraryPath is the javascript library path.
    * @returns {object} the Converter type.
    */
-  return function (jsLibraryPath) {
+  return function (htmlLibraryPath, jsLibraryPath) {
     /** @class @constructs MarkdownToHtml */
     function MarkdownToHtml() { }
 
@@ -47,23 +48,7 @@ var CreateConverter = (function() {
       // Build the HTML document that will load the showdown.js library.
       var document = new ActiveXObject('htmlFile');
       document.open();
-      document.IHTMLDocument2_write(
-        '<!DOCTYPE html>' +
-        '<html>' +
-        '<head>' +
-          '<meta charset="UTF-8" />' +
-          '<meta http-equiv="X-UA-Compatible" content="IE=edge" />' +
-          '<script type="text/javascript">' +
-            GetContent(jsLibraryPath) +
-            'function convertMarkdown() {' +
-              'document.body.innerHTML = (new showdown.Converter()).makeHtml(document.body.innerText);' +
-            '}' +
-          '</script>' +
-        '</head>' +
-        '<body>' +
-        '</body>' +
-        '</html>'
-      );
+      document.IHTMLDocument2_write(format(GetContent(htmlLibraryPath), GetContent(jsLibraryPath)));
       document.close();
       document.body.innerText = markdownContent;
       document.parentWindow.execScript('convertMarkdown()', 'javascript');

src/package.js
@@ -1,6 +1,6 @@
 /**
  * @file returns information about the resource files used by the project.
- * @version 0.0.1.7
+ * @version 0.0.1.8
  */
 
 /**
@@ -8,6 +8,7 @@
  * @property {string} Root is the project root path.
  * @property {string} ResourcePath is the project resources directory path.
  * @property {string} MenuIconPath is the shortcut menu icon path.
+ * @property {string} HtmlLibraryPath is the html file that embeds the JS library path.
  * @property {string} JsLibraryPath is the showdown.js library path.
  */
 
@@ -18,6 +19,7 @@ var Package = (function() {
     MenuIconPath: AssemblyLocation
   };
   resource.ResourcePath = FileSystem.BuildPath(resource.Root, 'rsc');
+  resource.HtmlLibraryPath = FileSystem.BuildPath(resource.ResourcePath, 'showdown.html');
   resource.JsLibraryPath = FileSystem.BuildPath(resource.ResourcePath, 'showdown.min.js');
   return resource;
 })();